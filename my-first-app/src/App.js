e5072c485a8acfc621b98b1cbe99a85bbfda0996
Gustavo Maia <109868838+gugarioms@users.noreply.github.com>
2022-11-16 20:45:32 -0300

    react

---
+++ my-first-app/src/App.js
@@ -0,0 +1,13 @@
+import Loremipsum from "./components/Loremipsum";
+import HelloWorld from "./hello";
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+      <Loremipsum />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-first-app/src/components/Loremipsum.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Loremipsum = () => {
+  return <p>Meu nome é Gustavo e estou aprendendo React</p>;
+};
+
+export default Loremipsum;
